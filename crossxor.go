fed81048e50a575c046b4c03e06b1ce7551bac39
Dominik Gawlik <dominik.gawlik1@gmail.com>
2022-10-03 06:53:09 +0200

    performance

--- crossxor.go
+++ crossxor.go
@@ -5,7 +5,6 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
-	"math/big"
 	"math/rand"
 	"time"
 	"unsafe"
@@ -30,123 +29,6 @@ type State struct {
 func hexToVector(x string) BitVector {
 	return *(*BitVector)(decode(x))
 }
-
-func (a BitVector) xor(b BitVector) BitVector {
-	var c BitVector
-
-	for i := 0; i < 16; i++ {
-		c[i] = a[i] ^ b[i]
-	}
-
-	return c
-}
-
-func (a BitVector) and(b BitVector) BitVector {
-	var c BitVector
-
-	for i := 0; i < 16; i++ {
-		c[i] = a[i] & b[i]
-	}
-
-	return c
-}
-
-func (a BitVector) or(b BitVector) BitVector {
-	var c BitVector
-
-	for i := 0; i < 16; i++ {
-		c[i] = a[i] | b[i]
-	}
-
-	return c
-}
-
-func (a BitVector) sum(b BitVector) BitVector {
-	var c BitVector
-
-	carry := 0
-	for i := 0; i < 16; i++ {
-		s := int(a[i]) + int(b[i]) + carry
-		carry = s / 256
-		c[i] = byte(s)
-	}
-
-	return c
-}
-
-func (a BitVector) sub(b BitVector) BitVector {
-	var c BitVector
-
-	var bigA big.Int
-	bigA.SetBytes(a[:])
-
-	var bigB big.Int
-	bigB.SetBytes(b[:])
-
-	var bigC big.Int
-
-	bigC.Sub(&bigB, &bigA)
-	bigC.FillBytes(c[:])
-
-	return c
-}
-
-// func (a BitVector) crossProd(b BitVector, i int) BitVector {
-
-// 	// if i >= 16 {
-// 	// 	return a.crossProdBig(b, i)
-// 	// } else if i == 2 {
-// 	// 	return a.crossProd2(b, i)
-// 	// } else if i == 4 {
-// 	// 	return a.crossProd4(b, i)
-// 	// } else {
-// 	// 	return a.crossProd8(b, i)
-// 	// }
-
-// 	// var result BitVector
-
-// 	// blocks := 128 / i
-// 	// blockSize := i
-
-// 	// for j := 0; j < blocks; j++ {
-// 	// 	for k := 0; k < blockSize/2; k++ {
-// 	// 		iSrc := j*blockSize + k
-// 	// 		iDest := j*blockSize + blockSize/2 + k
-
-// 	// 		if (!HasBit(a[iSrc/8], iSrc%8) && HasBit(b[iDest/8], iDest%8)) ||
-// 	// 			(HasBit(a[iSrc/8], iSrc%8) && !HasBit(b[iDest/8], iDest%8)) {
-// 	// 			result[iSrc/8] = SetBit(result[iSrc/8], iSrc%8)
-// 	// 		}
-
-// 	// 		if (!HasBit(b[iSrc/8], iSrc%8) && HasBit(a[iDest/8], iDest%8)) ||
-// 	// 			(HasBit(b[iSrc/8], iSrc%8) && !HasBit(a[iDest/8], iDest%8)) {
-// 	// 			result[iDest/8] = SetBit(result[iDest/8], iDest%8)
-// 	// 		}
-// 	// 	}
-// 	// }
-
-// 	// return result
-// }
-
-// func (a BitVector) crossProdBig(b BitVector, i int) BitVector {
-// 	var result BitVector
-
-// 	blocks := 128 / i
-// 	blockSize := i / 8
-
-// 	for j := 0; j < blocks; j++ {
-// 		for k := 0; k < blockSize/2; k++ {
-// 			iSrc := j*blockSize + k
-// 			iDest := j*blockSize + blockSize/2 + k
-
-// 			result[iSrc] = a[iSrc] ^ b[iDest]
-// 			result[iDest] = a[iDest] ^ b[iSrc]
-// 		}
-// 	}
-
-// 	return result
-
-// }
 
 func bootstrap(key BitVector) State {
 	Ks := (*[2]uint64)(unsafe.Pointer(&key[0]))
@@ -212,127 +94,6 @@ func (st *State) invPermuteSubstitute(a *BitVector) {
 	*a = result
 }
 
-func (a *BitVector) crossProd16(b *BitVector) {
-	var result BitVector
-
-	cA := (*[2]uint64)(unsafe.Pointer(&a[0]))
-	cB := (*[2]uint64)(unsafe.Pointer(&b[0]))
-	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
-
-	maskOdd := uint64(0b11111111_00000000_11111111_00000000_11111111_00000000_11111111_00000000)
-	maskEven := uint64(0b00000000_11111111_00000000_11111111_00000000_11111111_00000000_11111111)
-
-	for j := 0; j < 2; j++ {
-		cResult[j] |= (cA[j] ^ (cB[j] << 8)) & maskOdd
-		cResult[j] |= (cA[j] ^ (cB[j] >> 8)) & maskEven
-	}
-
-	*a = result
-}
-
-func (a *BitVector) crossProd32(b *BitVector) {
-	var result BitVector
-
-	cA := (*[2]uint64)(unsafe.Pointer(&a[0]))
-	cB := (*[2]uint64)(unsafe.Pointer(&b[0]))
-	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
-
-	maskOdd := uint64(0b11111111_11111111_00000000_00000000_11111111_11111111_00000000_00000000)
-	maskEven := uint64(0b00000000_00000000_11111111_11111111_00000000_00000000_11111111_11111111)
-
-	for j := 0; j < 2; j++ {
-		cResult[j] |= (cA[j] ^ (cB[j] << 16)) & maskOdd
-		cResult[j] |= (cA[j] ^ (cB[j] >> 16)) & maskEven
-	}
-
-	*a = result
-}
-
-func (a *BitVector) crossProd64(b *BitVector) {
-	var result BitVector
-
-	cA := (*[2]uint64)(unsafe.Pointer(&a[0]))
-	cB := (*[2]uint64)(unsafe.Pointer(&b[0]))
-	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
-
-	maskOdd := uint64(0b11111111_11111111_11111111_11111111_00000000_00000000_00000000_00000000)
-	maskEven := uint64(0b00000000_00000000_00000000_00000000_11111111_11111111_11111111_11111111)
-
-	for j := 0; j < 2; j++ {
-		cResult[j] |= (cA[j] ^ (cB[j] << 32)) & maskOdd
-		cResult[j] |= (cA[j] ^ (cB[j] >> 32)) & maskEven
-	}
-
-	*a = result
-}
-
-func (a *BitVector) crossProd128(b *BitVector) {
-	var result BitVector
-
-	cA := (*[2]uint64)(unsafe.Pointer(&a[0]))
-	cB := (*[2]uint64)(unsafe.Pointer(&b[0]))
-	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
-
-	cResult[0] = cA[0] ^ cB[1]
-	cResult[1] = cA[1] ^ cB[0]
-
-	*a = result
-}
-
-func (a *BitVector) crossProd2(b *BitVector) {
-	var result BitVector
-
-	cA := (*[2]uint64)(unsafe.Pointer(&a[0]))
-	cB := (*[2]uint64)(unsafe.Pointer(&b[0]))
-	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
-
-	maskOdd := uint64(0b10101010_10101010_10101010_10101010_10101010_10101010_10101010_10101010)
-	maskEven := uint64(0b01010101_01010101_01010101_01010101_01010101_01010101_01010101_01010101)
-
-	for j := 0; j < 2; j++ {
-		cResult[j] |= (cA[j] ^ (cB[j] << 1)) & maskOdd
-		cResult[j] |= (cA[j] ^ (cB[j] >> 1)) & maskEven
-	}
-
-	*a = result
-}
-
-func (a *BitVector) crossProd4(b *BitVector) {
-	var result BitVector
-
-	cA := (*[2]uint64)(unsafe.Pointer(&a[0]))
-	cB := (*[2]uint64)(unsafe.Pointer(&b[0]))
-	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
-
-	maskOdd := uint64(0b11001100_11001100_11001100_11001100_11001100_11001100_11001100_11001100)
-	maskEven := uint64(0b00110011_00110011_00110011_00110011_00110011_00110011_00110011_00110011)
-
-	for j := 0; j < 2; j++ {
-		cResult[j] |= (cA[j] ^ (cB[j] << 2)) & maskOdd
-		cResult[j] |= (cA[j] ^ (cB[j] >> 2)) & maskEven
-	}
-
-	*a = result
-}
-
-func (a *BitVector) crossProd8(b *BitVector) {
-	var result BitVector
-
-	cA := (*[2]uint64)(unsafe.Pointer(&a[0]))
-	cB := (*[2]uint64)(unsafe.Pointer(&b[0]))
-	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
-
-	maskOdd := uint64(0b11110000_11110000_11110000_11110000_11110000_11110000_11110000_11110000)
-	maskEven := uint64(0b00001111_00001111_00001111_00001111_00001111_00001111_00001111_00001111)
-
-	for j := 0; j < 2; j++ {
-		cResult[j] |= (cA[j] ^ (cB[j] << 4)) & maskOdd
-		cResult[j] |= (cA[j] ^ (cB[j] >> 4)) & maskEven
-	}
-
-	*a = result
-}
-
 var SOURCE = rand.NewSource(time.Now().UnixNano())
 var RNG = rand.New(SOURCE)
 
@@ -363,18 +124,7 @@ func encode(payload []byte) string {
 	return buf.String()
 }
 
-// not homomorphic
-func (st *State) doEncrypt(x BitVector) BitVector {
-
-	state := x
-	st.permuteSubstitute(&state)
-
-	var result BitVector
-
-	cA := (*[2]uint64)(unsafe.Pointer(&state[0]))
-	cB := (*[2]uint64)(unsafe.Pointer(&st.Key[0]))
-	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
-
+func encryptLong(x, key uint64) uint64 {
 	maskOdd2 := uint64(0b10101010_10101010_10101010_10101010_10101010_10101010_10101010_10101010)
 	maskEven2 := uint64(0b01010101_01010101_01010101_01010101_01010101_01010101_01010101_01010101)
 
@@ -393,43 +143,117 @@ func (st *State) doEncrypt(x BitVector) BitVector {
 	maskOdd64 := uint64(0b11111111_11111111_11111111_11111111_00000000_00000000_00000000_00000000)
 	maskEven64 := uint64(0b00000000_00000000_00000000_00000000_11111111_11111111_11111111_11111111)
 
-	for j := 0; j < 2; j++ {
-		cResult[j] |= (cA[j] ^ (cB[j] << 1)) & maskOdd2
-		cResult[j] |= (cA[j] ^ (cB[j] >> 1)) & maskEven2
+	result := uint64(0)
+	state := x
 
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
+	result |= (state ^ (key << 1)) & maskOdd2
+	result |= (state ^ (key >> 1)) & maskEven2
 
-		cResult[j] |= (cA[j] ^ (cB[j] << 2)) & maskOdd4
-		cResult[j] |= (cA[j] ^ (cB[j] >> 2)) & maskEven4
+	state = result
+	result = uint64(0)
 
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
+	result |= (state ^ (key << 2)) & maskOdd4
+	result |= (state ^ (key >> 2)) & maskEven4
 
-		cResult[j] |= (cA[j] ^ (cB[j] << 4)) & maskOdd8
-		cResult[j] |= (cA[j] ^ (cB[j] >> 4)) & maskEven8
+	state = result
+	result = uint64(0)
 
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
+	result |= (state ^ (key << 4)) & maskOdd8
+	result |= (state ^ (key >> 4)) & maskEven8
 
-		cResult[j] |= (cA[j] ^ (cB[j] << 8)) & maskOdd16
-		cResult[j] |= (cA[j] ^ (cB[j] >> 8)) & maskEven16
+	state = result
+	result = uint64(0)
 
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
+	result |= (state ^ (key << 8)) & maskOdd16
+	result |= (state ^ (key >> 8)) & maskEven16
 
-		cResult[j] |= (cA[j] ^ (cB[j] << 16)) & maskOdd32
-		cResult[j] |= (cA[j] ^ (cB[j] >> 16)) & maskEven32
+	state = result
+	result = uint64(0)
 
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
+	result |= (state ^ (key << 16)) & maskOdd32
+	result |= (state ^ (key >> 16)) & maskEven32
 
-		cResult[j] |= (cA[j] ^ (cB[j] << 32)) & maskOdd64
-		cResult[j] |= (cA[j] ^ (cB[j] >> 32)) & maskEven64
+	state = result
+	result = uint64(0)
 
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
-	}
+	result |= (state ^ (key << 32)) & maskOdd64
+	result |= (state ^ (key >> 32)) & maskEven64
+
+	return result
+}
+
+func decryptLong(x, key uint64) uint64 {
+	maskOdd2 := uint64(0b10101010_10101010_10101010_10101010_10101010_10101010_10101010_10101010)
+	maskEven2 := uint64(0b01010101_01010101_01010101_01010101_01010101_01010101_01010101_01010101)
+
+	maskOdd4 := uint64(0b11001100_11001100_11001100_11001100_11001100_11001100_11001100_11001100)
+	maskEven4 := uint64(0b00110011_00110011_00110011_00110011_00110011_00110011_00110011_00110011)
+
+	maskOdd8 := uint64(0b11110000_11110000_11110000_11110000_11110000_11110000_11110000_11110000)
+	maskEven8 := uint64(0b00001111_00001111_00001111_00001111_00001111_00001111_00001111_00001111)
+
+	maskOdd16 := uint64(0b11111111_00000000_11111111_00000000_11111111_00000000_11111111_00000000)
+	maskEven16 := uint64(0b00000000_11111111_00000000_11111111_00000000_11111111_00000000_11111111)
+
+	maskOdd32 := uint64(0b11111111_11111111_00000000_00000000_11111111_11111111_00000000_00000000)
+	maskEven32 := uint64(0b00000000_00000000_11111111_11111111_00000000_00000000_11111111_11111111)
+
+	maskOdd64 := uint64(0b11111111_11111111_11111111_11111111_00000000_00000000_00000000_00000000)
+	maskEven64 := uint64(0b00000000_00000000_00000000_00000000_11111111_11111111_11111111_11111111)
+
+	result := uint64(0)
+	state := x
+
+	result |= (state ^ (key << 32)) & maskOdd64
+	result |= (state ^ (key >> 32)) & maskEven64
+
+	state = result
+	result = uint64(0)
+
+	result |= (state ^ (key << 16)) & maskOdd32
+	result |= (state ^ (key >> 16)) & maskEven32
+
+	state = result
+	result = uint64(0)
+
+	result |= (state ^ (key << 8)) & maskOdd16
+	result |= (state ^ (key >> 8)) & maskEven16
+
+	state = result
+	result = uint64(0)
+
+	result |= (state ^ (key << 4)) & maskOdd8
+	result |= (state ^ (key >> 4)) & maskEven8
+
+	state = result
+	result = uint64(0)
+
+	result |= (state ^ (key << 2)) & maskOdd4
+	result |= (state ^ (key >> 2)) & maskEven4
+
+	state = result
+	result = uint64(0)
+
+	result |= (state ^ (key << 1)) & maskOdd2
+	result |= (state ^ (key >> 1)) & maskEven2
+
+	return result
+}
+
+// not homomorphic
+func (st *State) doEncrypt(x BitVector) BitVector {
+
+	state := x
+	st.permuteSubstitute(&state)
+
+	var result BitVector
+
+	cA := (*[2]uint64)(unsafe.Pointer(&state[0]))
+	cB := (*[2]uint64)(unsafe.Pointer(&st.Key[0]))
+	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
+
+	cResult[0], cResult[1] = encryptLong(cA[0], cB[0]), encryptLong(cA[1], cB[1])
+	cA[0], cA[1] = cResult[0], cResult[1]
 
 	cResult[0] = cA[0] ^ cB[1]
 	cResult[1] = cA[1] ^ cB[0]
@@ -446,64 +270,12 @@ func (st *State) doDecrypt(x BitVector) BitVector {
 	cB := (*[2]uint64)(unsafe.Pointer(&st.Key[0]))
 	cResult := (*[2]uint64)(unsafe.Pointer(&result[0]))
 
-	maskOdd2 := uint64(0b10101010_10101010_10101010_10101010_10101010_10101010_10101010_10101010)
-	maskEven2 := uint64(0b01010101_01010101_01010101_01010101_01010101_01010101_01010101_01010101)
-
-	maskOdd4 := uint64(0b11001100_11001100_11001100_11001100_11001100_11001100_11001100_11001100)
-	maskEven4 := uint64(0b00110011_00110011_00110011_00110011_00110011_00110011_00110011_00110011)
-
-	maskOdd8 := uint64(0b11110000_11110000_11110000_11110000_11110000_11110000_11110000_11110000)
-	maskEven8 := uint64(0b00001111_00001111_00001111_00001111_00001111_00001111_00001111_00001111)
-
-	maskOdd16 := uint64(0b11111111_00000000_11111111_00000000_11111111_00000000_11111111_00000000)
-	maskEven16 := uint64(0b00000000_11111111_00000000_11111111_00000000_11111111_00000000_11111111)
-
-	maskOdd32 := uint64(0b11111111_11111111_00000000_00000000_11111111_11111111_00000000_00000000)
-	maskEven32 := uint64(0b00000000_00000000_11111111_11111111_00000000_00000000_11111111_11111111)
-
-	maskOdd64 := uint64(0b11111111_11111111_11111111_11111111_00000000_00000000_00000000_00000000)
-	maskEven64 := uint64(0b00000000_00000000_00000000_00000000_11111111_11111111_11111111_11111111)
-
 	cResult[0] = cA[0] ^ cB[1]
 	cResult[1] = cA[1] ^ cB[0]
 	cA[0], cA[1] = cResult[0], cResult[1]
 	cResult[0], cResult[1] = uint64(0), uint64(0)
 
-	for j := 0; j < 2; j++ {
-		cResult[j] |= (cA[j] ^ (cB[j] << 32)) & maskOdd64
-		cResult[j] |= (cA[j] ^ (cB[j] >> 32)) & maskEven64
-
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
-
-		cResult[j] |= (cA[j] ^ (cB[j] << 16)) & maskOdd32
-		cResult[j] |= (cA[j] ^ (cB[j] >> 16)) & maskEven32
-
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
-
-		cResult[j] |= (cA[j] ^ (cB[j] << 8)) & maskOdd16
-		cResult[j] |= (cA[j] ^ (cB[j] >> 8)) & maskEven16
-
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
-
-		cResult[j] |= (cA[j] ^ (cB[j] << 4)) & maskOdd8
-		cResult[j] |= (cA[j] ^ (cB[j] >> 4)) & maskEven8
-
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
-
-		cResult[j] |= (cA[j] ^ (cB[j] << 2)) & maskOdd4
-		cResult[j] |= (cA[j] ^ (cB[j] >> 2)) & maskEven4
-
-		cA[j] = cResult[j]
-		cResult[j] = uint64(0)
-
-		cResult[j] |= (cA[j] ^ (cB[j] << 1)) & maskOdd2
-		cResult[j] |= (cA[j] ^ (cB[j] >> 1)) & maskEven2
-
-	}
+	cResult[0], cResult[1] = decryptLong(cA[0], cB[0]), decryptLong(cA[1], cB[1])
 
 	state = result
 	st.invPermuteSubstitute(&state)
